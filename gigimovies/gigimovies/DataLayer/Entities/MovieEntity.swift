73fd5d2026b2b43d77810043360b224720f9cff7
Guido Fabio <guidof@nimble.la>
2021-09-02 14:44:09 +0200

    ADD: Movie entity

---
+++ gigimovies/gigimovies/DataLayer/Entities/MovieEntity.swift
@@ -0,0 +1,30 @@
+//
+//  MovieEntity.swift
+//  Gigimovies
+//
+//  Created by Guido Fabio on 02/09/2021.
+//
+
+import Foundation
+
+struct MovieEntity: Codable {
+    let adult: Bool
+    let backdrop_path: String
+    let genre_ids: [Int]
+    let id: Int
+    let original_language: String
+    let original_title: String
+    let overview: String
+    let popularity: Double
+    let poster_path: String
+    let release_date: String
+    let title: String
+    let video: Bool
+    let vote_average: Int
+    let vote_count: Int
+}
+
+struct Results: Codable {
+    let page: Int
+    let results: [MovieEntity]
+}
